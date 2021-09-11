bc8547b5f341da40b42ad444cdc79e9d8036076a
KrazyCo <conorstrickland@hotmail.com>
2021-09-11 16:58:26 +0100

    commit

--- 02 Selection task/Periodic table problem.py
+++ 02 Selection task/Periodic table problem.py
@@ -1,9 +1,25 @@
 # I didn't know how to seach through the dictionary for groups, so I didn't include this
 
 elements = {
-    "Lithium": ("Lithium", "Li", "6.94", "Alkali metals"),
-    "Li": ("Lithium", "Li", "6.94", "Alkali metals"),
-    "Sodium": ("Sodium", "Na", "22.990", "Alkali metals"),
-    "Na": ("Sodium", "Na", "22.990", "Alkali metals")
+    "lithium": ("Lithium", "Li", "6.94", "Alkali metals"),
+    "li": ("Lithium", "Li", "6.94", "Alkali metals"),
+    "sodium": ("Sodium", "Na", "22.990", "Alkali metals"),
+    "na": ("Sodium", "Na", "22.990", "Alkali metals"),
+    "potassium": ("Potassium", "K", "39.098", "Alkali metals"),
+    "k": ("Potassium", "K", "39.098", "Alkali metals"),
+    "helium": ("Helium", "He", "4.003", "Nobel gasses"),
+    "he": ("Helium", "He", "4.003", "Nobel gasses"),
+    "neon": ("Neon", "Ne", "20.180", "Nobel gasses"),
+    "ne": ("Neon", "Ne", "20.180", "Nobel gasses"),
+    "argon": ("Argon", "Ar", "39.95", "Nobel gasses"),
+    "ar": ("Argon", "Ar", "39.95", "Nobel gasses"),
 }
-print(elements["Lithium"])
+print("Compatible elements: Lithium, Sodium, Potassium, Helium, Neon, Argon\n")
+userInput = str(input("Enter the symbol or name of an element: "))
+
+print('''
+Element: %s(%s)
+Atomic weight: %s
+Group: %s''' % (elements[userInput.lower()][0], elements[userInput.lower()][1], elements[userInput.lower()][2], elements[userInput.lower()][3]))
+    
+    # elements[userInput.lower()][0])
