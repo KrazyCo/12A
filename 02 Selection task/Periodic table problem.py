2ca846c9cfd127f11fc7ef4aae4063d57454eee2
KrazyCo <conorstrickland@hotmail.com>
2021-09-11 16:45:40 +0100

    commit

---
+++ 02 Selection task/Periodic table problem.py
@@ -0,0 +1,9 @@
+# I didn't know how to seach through the dictionary for groups, so I didn't include this
+
+elements = {
+    "Lithium": ("Lithium", "Li", "6.94", "Alkali metals"),
+    "Li": ("Lithium", "Li", "6.94", "Alkali metals"),
+    "Sodium": ("Sodium", "Na", "22.990", "Alkali metals"),
+    "Na": ("Sodium", "Na", "22.990", "Alkali metals")
+}
+print(elements["Lithium"])
