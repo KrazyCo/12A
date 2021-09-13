863f9b9f63692bc8a85d2e46946163cd64e885f8
KrazyCo <conorstrickland@hotmail.com>
2021-09-13 13:22:50 +0100

    commit

--- 02 Selection task/GUI Portfolio grade problem.py
+++ 02 Selection task/GUI Portfolio grade problem.py
@@ -1,9 +1,19 @@
-from guizero import App, TextBox, Text, Box
+from guizero import App, TextBox, Text, Box, PushButton
 
 app = App(title="Portfolio grade problem")
 
 def calculate():
-    pass
+    grades = [2, 4, 13, 22, 31, 41, 54, 67, 80]
+    total = int(aTextBox.value) + int(dTextBox.value) + int(iTextBox.value) + int(eTextBox.value)
+    if int(total) < 2:
+        grade = "U"
+        toNextGrade = 2 - total
+    for i in range(len(grades)):
+        if int(total) < grades[i]:
+            grade = str(i)
+            toNextGrade = grades[i] - total
+            break
+    outputText.value = "Your total mark was %s\n\nYou achived grade %s, and you needed\n%s more marks to achive the next grade." % (str(total), grade, toNextGrade)
 
 inputBox = Box(app, align="top", border=True)
 aBox = Box(inputBox, align="left", border=True)
@@ -20,5 +30,8 @@ aTextBox = TextBox(aBox)
 dTextBox = TextBox(dBox)
 iTextBox = TextBox(iBox)
 eTextBox = TextBox(eBox)
+
+submitButton = PushButton(app, text="Calculate", command=calculate)
+outputText = Text(app)
 
 app.display()
