c7c3a231ff7af87dcc47c88c87683c836ee41668
KrazyCo <conorstrickland@hotmail.com>
2021-09-17 11:02:14 +0100

    commit

--- Intro to python/Challanges/R@nd0m P@ssw0rd.py
+++ Intro to python/Challanges/R@nd0m P@ssw0rd.py
@@ -1,11 +1,28 @@
 import random
 import string
 
-letters = list(string.ascii_lowercase)
-numbers = list(string.digits)
-symbols = list(string.punctuation)
+chars = []
+password = ""
 
-passwordLength = int(input("How long do you want your password to be? "))
+
+passwordLength = int(input("How many characters do you want in your password? "))
 
 print("Enter a 1 for yes, 0 for no")
-if 
+if int(input("Do you want letters in your password? ")) == 1:
+    for i in list(string.ascii_lowercase):
+        chars.append(i)
+    if int(input("Do you want capital letters in your password? ")) == 1:
+        for i in list(string.ascii_uppercase):
+            chars.append(i)
+if int(input("Do you want numbers in your password? ")) == 1:
+    for i in list(string.digits):
+        chars.append(i)
+if int(input("Do you want symbols in your password? ")) == 1:
+    for i in list(string.punctuation):
+        chars.append(i)
+
+for i in range(passwordLength):
+    password += str(chars[random.randint(0,len(chars)-1)])
+    
+print("Your password is:")
+print(password)
