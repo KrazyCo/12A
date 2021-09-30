ad7b7c77fde4eb0ead99db35deb1477226d0c32b
KrazyCo <conorstrickland@hotmail.com>
2021-09-30 08:51:03 +0100

    commit

---
+++ Intro to python/Subroutines/Starter 2 lines.py
@@ -0,0 +1,2 @@
+timesTable = int(input("What timestable would you like to see: "))
+for i in range(int(input("What number would you like to end at: "))):print("%s x %s = %s" % (i+1, timesTable, (i+1)*timesTable))
