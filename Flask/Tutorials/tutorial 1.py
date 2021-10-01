d00ea6c9af5b1c831f650544be4d1d70708ee0e7
KrazyCo <conorstrickland@hotmail.com>
2021-10-01 12:10:03 +0100

    commit

---
+++ Flask/Tutorials/tutorial 1.py
@@ -0,0 +1,25 @@
+# https://www.youtube.com/watch?v=mqhxxeeTbu0&ab_channel=TechWithTim
+
+from flask import Flask, redirect, url_for, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home(name):
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", usr=user))
+    else:
+        return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
