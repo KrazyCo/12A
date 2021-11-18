8a6d0498c596301fbf21a3716696529396d0938d
KrazyCo <conorstrickland@hotmail.com>
2021-11-18 11:15:45 +0000

    hate this code

--- Homework/File handling exam question/main.py
+++ Homework/File handling exam question/main.py
@@ -9,7 +9,7 @@ def readMessage(fileName):
 print(readMessage("file.txt"))
 
 
-# question 2a
+# question 2
 
 def getText(fileName):
     file = open(fileName, "r")
@@ -24,7 +24,34 @@ def fullStop(fileName):
             capitalNeeded = True
             count = 1
             while capitalNeeded:
-                if ord(message[i+count]) => 97 and ord(message[i+count] =< 122:
-                    print(message[i+count])
-                    message[i+count] = message[i+count]
+                try:
+                    message[i+count] # checking if out of range
+                except:
                     capitalNeeded = False
+                else:
+                    if message[i+count] != "." and message[i+count] != " ":
+                        message = message[:i+count] + message [i+count].upper() + message[i+count+1:]
+                        capitalNeeded = False
+                    else:
+                        count += 1
+    print(f"{message = }")
+    file = open(fileName, "w")
+    file.write(message)
+    file.close()
+
+fullStop("fullstop.txt")
+
+# question 3
+
+def contains(string1, string2):
+    for i in range(len(string1)):
+        if string1[i:i+len(string2)] == string2:
+            return True
+    return False
+
+contains1 = contains("Hello", "Hell")
+print(f"{contains1 = }")                        # contains in it
+contains2 = contains("Why hello there", "hi")
+print(f"{contains2 = }")                        # doesnt contain it
+contains3 = contains("joe biden", "joe biden")
+print(f"{contains3 = }")                        # identical
