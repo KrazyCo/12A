6793711673b95ca02b913d77e12c19eaf4ff664d
KrazyCo <conorstrickland@hotmail.com>
2021-12-01 12:23:12 +0000

    stupid area

--- Decomposition/Room painter.py
+++ Decomposition/Room painter.py
@@ -8,7 +8,11 @@ def heightInput():
     return int(input("Enter the height of the room in meters: "))
 
 def calculate(width, length, height):
-    return width * length * height
+    total = 0
+    total += width * length
+    total += width * height * 2
+    total += length * height * 2
+    return total
 
 def output(area):
     print(f"The area of the room is {area} meters squared, so you need {area} liters of paint.")
