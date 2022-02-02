18a32406c9ceff57d7ea2eb2d2ae23b9a4970904
KrazyCo <conorstrickland@hotmail.com>
2022-02-02 12:03:52 +0000

    LETS GOOOOOO

---
+++ Intro to python/Stacks/stack.py
@@ -0,0 +1,36 @@
+class Stack:
+    def __init__(self,length):
+        self.length = length
+        self.index = 0
+        self.stack = [""]*length
+    
+    def push(self, arg):
+        if self.index >= self.length:
+            print("Stack is full, cancelling push")
+            return 
+        self.stack[self.index] = arg
+        self.index += 1
+
+    def pop(self):
+        if self.index == 0:
+            print("Stack is empty, cancelling pop")
+            return
+        self.index -= 1
+        return self.stack[self.index]
+
+    def printStack(self):
+        for i in range(self.length-1, self.index-1, -1):
+            print(str(i) + ":")
+        for i in range(self.index-1, -1, -1):
+            print(str(i) + ":", self.stack[i])
+
+if __name__ == "__main__":
+    stack = Stack(10)
+    while True:
+        userInput = str(input("0 for push, 1 for pop, 2 for printStack: "))
+        if userInput[0] == "0":
+            stack.push(userInput[2:])
+        if userInput[0] == "1":
+            print(stack.pop())
+        if userInput[0] == "2":
+            stack.printStack()
