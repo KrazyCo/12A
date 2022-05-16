831923e251aff456a091fdaf74ce0e8345b11e8f
KrazyCo <conorstrickland@hotmail.com>
2022-05-16 10:18:56 +0100

    dude no way is this like github replit pushing? omg no way dude what is this 50 char limit is that like just a warning or is that actually a thing

--- Speeding project/main.py
+++ Speeding project/main.py
@@ -1,3 +1,22 @@
+def getInput():
+    ageInput = 0
+    while ageInput == 0:
+        try:
+            ageInput = int(input("Input the age of the car, enter 999 to end: "))
+        except:
+            print("You must input an integer")
+    if ageInput == 999:
+        return False
+    speedInput = 0
+    while speedInput == 0:
+        try:
+            speedInput = int(input("Input the speed of the car, enter 999 to end: "))
+        except:
+            print("You must input an integer")
+    if speedInput == 999:
+        return False
+    return [ageInput, speedInput]
+
 def percentages(data):
     speeding = 0
     for i in range(len(data)):
@@ -6,5 +25,13 @@ def percentages(data):
     percent = speeding/len(data)*100
     return percent
 
-data = [[5, True], [3, False], [7, True], [2, False], [9, False], [1, False], [6, False], [1, False], [2, True], [3, False], [10, True], [2, False], [7, True], [5, False]]
-print(percentages(data))
+
+
+# data = [[5, True], [3, False], [7, True], [2, False], [9, False], [1, False], [6, False], [1, False], [2, True], [3, False], [10, True], [2, False], [7, True], [5, False]]
+# print(percentages(data))
+
+userInput = getInput()
+if userInput:
+    print(userInput)
+else:
+    print("User wants to end")
