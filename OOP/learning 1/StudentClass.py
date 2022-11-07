36e3afb7d187635640eda05fe9201c128ed0b809
KrazyCo <conorstrickland@hotmail.com>
2022-11-07 16:31:23 +0000

    LETS GO HOMEEEE

--- OOP/learning 1/StudentClass.py
+++ OOP/learning 1/StudentClass.py
@@ -1,3 +1,15 @@
 class Student():
     def __init__(self, name):
         self.name = name
+
+    def praise(self):
+        return f"Well done {self.name}, you've done really well!"
+
+    def reassurance(self):
+        return f"Keep going {self.name}, you're never gonna achive your goals!"
+
+    def feedback(self, grade):
+        if grade > 50:
+            return self.praise()
+        else:
+            return self.reassurance()
--- OOP/learning 1/main.py
+++ OOP/learning 1/main.py
@@ -2,3 +2,6 @@ from StudentClass import Student
 
 newStudent = Student("jeff")
 print(f"{newStudent.name = }")
+print(f"{newStudent.feedback(69) = }")
+print(f"{newStudent.feedback(13) = }")
+
