cc9ba2b8898d8614fac5986c92b0f760635bbf0b
KrazyCo <conorstrickland@hotmail.com>
2022-11-07 15:12:14 +0000

    i forgor

---
+++ random stuff/examstuff.py
@@ -0,0 +1,11 @@
+# textFile = open("random stuff/Sales.txt")
+# numbers = textFile.read().split("\n")
+# print(f"{numbers = }")
+# total = 0
+# for num in numbers:
+#     total += num
+# print(f"{num = }")
+
+lis = ["ahh", "jharaebr"]
+# print(f"{lis[] = }")
+print(lis)
