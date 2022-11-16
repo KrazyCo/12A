f12da6de967ca8a3062284526b74e0aca1251b81
KrazyCo <conorstrickland@hotmail.com>
2022-11-16 12:23:06 +0000

    well this doesnt work yet, butttttttt

--- Sorting Algorithms/quick sort/main.py
+++ Sorting Algorithms/quick sort/main.py
@@ -1,11 +1,7 @@
 def quicksort(array):
-    if len(array) <= 1: return array
-    pivot = array.pop(0)
-    leftArray, rightArray = [], []
-    for i in range(0,len(array)): 
-        if array[i] <= pivot: leftArray.append(array[i]) 
-        else: rightArray.append(array[i])
-    return quicksort(leftArray) + [pivot] + quicksort(rightArray)
+    leftArray, rightArray, pivot, end, oarray, array = ([], [], array.pop(0) if len(array)>1 else 0, False if len(array)>1 else True, array, array if len(array)>1 else [])
+    for i in range(0,len(array)): leftArray.append(array[i]) if array[i] <= pivot else rightArray.append(array[i])
+    return quicksort(leftArray) + [pivot] + quicksort(rightArray) if not end else oarray
 
 if __name__ == "__main__":
     # test code
@@ -18,7 +14,7 @@ if __name__ == "__main__":
                 [9, 8, 7, 6, 5, 4, 3, 2, 1],
                 [1, 18, 7, 9, 8, 4, 2, 6, 0, 14, 19, 16, 11, 12, 15, 5, 13, 17, 3, 10]]
 
-    print(f"{testarrs[-1] = }")
+    # print(f"{testarrs[-1] = }")
 
-    for array in testarrs:
-        print(array, "-->", quicksort(array))
+    for arr in testarrs:
+        print(arr, "-->", quicksort(arr))
