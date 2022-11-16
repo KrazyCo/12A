2eebe8141952d56124a5b2dd749eb2f853abe298
KrazyCo <conorstrickland@hotmail.com>
2022-11-16 14:47:59 +0000

    5 lines is the best youre getting i fought python for 2 hours to get just 1 line and it doesnt work im crying

--- Sorting Algorithms/quick sort/main.py
+++ Sorting Algorithms/quick sort/main.py
@@ -1,5 +1,6 @@
 def quicksort(array):
-    leftArray, rightArray, pivot, end, oarray, array = ([], [], array.pop(0) if len(array)>1 else 0, False if len(array)>1 else True, array, array if len(array)>1 else [])
+    if len(array) <= 1: return array
+    leftArray, rightArray, pivot, end, oarray, array = ([], [], array.pop(0) if len(array)>=1 else 0, False, array, array)
     for i in range(0,len(array)): leftArray.append(array[i]) if array[i] <= pivot else rightArray.append(array[i])
     return quicksort(leftArray) + [pivot] + quicksort(rightArray) if not end else oarray
 
@@ -14,7 +15,5 @@ if __name__ == "__main__":
                 [9, 8, 7, 6, 5, 4, 3, 2, 1],
                 [1, 18, 7, 9, 8, 4, 2, 6, 0, 14, 19, 16, 11, 12, 15, 5, 13, 17, 3, 10]]
 
-    # print(f"{testarrs[-1] = }")
-
-    for arr in testarrs:
-        print(arr, "-->", quicksort(arr))
+    for array in testarrs:
+        print(array, "-->", quicksort(array))
