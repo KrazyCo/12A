19987da6cd63e740e258167e39a81f8799604512
KrazyCo <conorstrickland@hotmail.com>
2022-11-16 11:32:09 +0000

    i want my vscode pets back :(

--- Sorting Algorithms/quick sort/main.py
+++ Sorting Algorithms/quick sort/main.py
@@ -17,18 +17,18 @@ def quicksort(array):
     array = quicksort(leftArray) + [pivot] + quicksort(rightArray)
     return array
 
+if __name__ == "__main__":
+    # test code
+    testarrs = [[],
+                [2],
+                [1, 5],
+                [5, 1],
+                [1, 6, 2],
+                [6, 2, 1],
+                [9, 8, 7, 6, 5, 4, 3, 2, 1],
+                [1, 18, 7, 9, 8, 4, 2, 6, 0, 14, 19, 16, 11, 12, 15, 5, 13, 17, 3, 10]]
 
-# test code
-testarrs = [[],
-            [2],
-            [1, 5],
-            [5, 1],
-            [1, 6, 2],
-            [6, 2, 1],
-            [9, 8, 7, 6, 5, 4, 3, 2, 1],
-            [1, 18, 7, 9, 8, 4, 2, 6, 0, 14, 19, 16, 11, 12, 15, 5, 13, 17, 3, 10]]
+    print(f"{testarrs[-1] = }")
 
-print(f"{testarrs[-1] = }")
-
-for array in testarrs:
-    print(array, "-->", quicksort(array))
+    for array in testarrs:
+        print(array, "-->", quicksort(array))
