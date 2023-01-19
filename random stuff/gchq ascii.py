9a4bef41bb458282742aa33f33af32f0de2fa51d
KrazyCo <conorstrickland@hotmail.com>
2023-01-19 14:40:20 +0000

    leave

--- random stuff/gchq ascii.py
+++ random stuff/gchq ascii.py
@@ -1,9 +1,19 @@
-data = "xibu4xpset beesftt jt pvucpbse.hsje.sfkpjot"
+data = "doov zhoo wkdw hqgv zhoo"
 
-for i in range(-10, 10):
+for i in range(0, 26):
     output = ""
     for character in data:
-        output += chr(ord(character)+i)
+        if ord(character) == 32:
+            output += " "
+        else:
+            if ord(character) < 65 or ord(character) > 122:
+                output += character
+            elif ord(character)+i > 122:
+                output += chr(ord(character)+i-26)
+            elif ord(character)+i < 97:
+                output += chr(ord(character)+i+26)
+            else:
+                output += chr(ord(character)+i)
     print(f"{i = }")
         # print(f"{ord(character) = }")
     print(output)
