ecb682c74aab88c2becf2844e90c8ceebbfb2c56
KrazyCo <conorstrickland@hotmail.com>
2023-02-24 12:07:21 +0000

    comments go ahhh

--- Data structures/Trees/Binary trees/Traverses.py
+++ Data structures/Trees/Binary trees/Traverses.py
@@ -47,37 +47,51 @@ print("\nPost-order procedural")
 postOrderTraversalProcedural(0)
 
 # object oriented
-class Node:
-	def __init__(self, name):
-		self.__name = name
+class Node: # class definition - no inheritance - capatilised
+	def __init__(self, name): # constructor method - automatticly run at object instantiation/moment of creation - public
+		# __init__ is the identifier
+		# self is the object itself - name is the parameter
+		self.__name = name # data is variable
+		# = is assignment 
+		# __name is private attribute 
 	
-	def setLeft(self, left):
+    # all examples of encapsulation - using public methods to access private attributes
+	def setLeft(self, left): # setter method
 		self.__left = left
 
-	def setRight(self, right):
+	def setRight(self, right): # setter method
 		self.__right = right
 
-	def getLeft(self):
+	def getLeft(self): # getter method 
 		return self.__left
 
-	def getRight(self):
+	def getRight(self): # getter method
 		return self.__right
 
-	def getName(self):
+	def getName(self): # getter method
 		return self.__name
 
-oopTree = []
+# from nodeClass import Node (example of importing node from nodeClass.py)
+# nodeClass is libary - Node is class
+
+# tree is 2d list - as has different data types
+# "tree" is identifier
+
+oopTree = [] # oopTree is identifier to which an empty list is assigned - will be list of objects
+
+for i in range(len(tree)): # for loop - counter controlled iteration - i is counter - in range means start at 0 and go up to but length of tree -1
+	oopTree.append(Node(tree[i][1])) # append is list method
+	# Node(tree[i][1]) is object instantiation, setting name to tree[i][1] - tree[i][1] is the name of the node
 
 for i in range(len(tree)):
-	oopTree.append(Node(tree[i][1]))
-
-for i in range(len(tree)):
-    oopTree[i].setLeft(oopTree[tree[i][0]] if tree[i][0] != -1 else None)
+    oopTree[i].setLeft(oopTree[tree[i][0]] if tree[i][0] != -1 else None) # if is selection - branching based on condition 
+    # setLeft is setter method - oopTree is list, index is tree[i][0] which is left pointer 
+    # takes appropriate left hand pointer from the 2d list tree and assigns the left pointer of the object to the correct object in oopTree
     oopTree[i].setRight(oopTree[tree[i][2]] if tree[i][2] != -1 else None)
 
-def inOrderOOP(currentNode):
-	if currentNode.getLeft(): inOrderOOP(currentNode.getLeft())
-	print(currentNode.getName())
+def inOrderOOP(currentNode): # defines a procedure - inOrderOOP is identifier - currentNode is parameter
+	if currentNode.getLeft(): inOrderOOP(currentNode.getLeft()) # recursive call - condition is if currentNode has a left pointer - condition must evaluate to true or false
+	print(currentNode.getName()) # getName is getter method - currentNode is object
 	if currentNode.getRight(): inOrderOOP(currentNode.getRight())
         
 def preOrderOOP(currentNode):
@@ -96,8 +110,8 @@ def postOrderOOP(currentNode):
 #     print(f"Right: {oopTree[i].getRight().getName() if oopTree[i].getRight() else None}")
 #     print()
 
-print("\nIn-order object oriented")
-inOrderOOP(oopTree[startNode])
+print("\nIn-order object oriented") # outputs to the user - "" is string
+inOrderOOP(oopTree[startNode]) # procedure call - inOrderOOP is identifier - oopTree[startNode] is argument - root node is oopTree[0] - startNode is 0
 
 print("\nPre-order object oriented")
 preOrderOOP(oopTree[startNode])
