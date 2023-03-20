34a600748d5ff5a7d17f0b95bc7f9dad64ee99ef
KrazyCo <conorstrickland@hotmail.com>
2023-03-20 21:53:08 +0000

    we do a little trolling

--- random stuff/conut thing.py
+++ random stuff/conut thing.py
@@ -1,9 +1,25 @@
-import time
+# import time
 
-startTime = time.time()
+# startTime = time.time()
 
-for count in range(0,1000000):
-    print(count)
-print("finished")
-print(time.time() - startTime)
-print(f"1 billion would take {(time.time() - startTime) * 1000} seconds")
+# for count in range(0,1000000):
+#     print(count)
+# print("finished")
+# print(time.time() - startTime)
+# print(f"1 billion would take {(time.time() - startTime) * 1000} seconds")
+
+number = 2
+
+for i in range(0,410000):
+    number = number * 2
+
+print(number)
+print(len(str(number)))
+
+# number = 2
+
+# for i in range(0,30): # this loop will never finish lmao
+#     number = number * number
+#     print(number)
+#     print(f"{i = }")
+#     print(f"{len(str(number)) = }")
