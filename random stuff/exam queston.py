86a6ce5caee49b2abef24dc8653050d74005a8fd
KrazyCo <conorstrickland@hotmail.com>
2023-05-11 14:31:47 +0100

    nvm lol

--- random stuff/exam queston.py
+++ random stuff/exam queston.py
@@ -4,6 +4,6 @@ class Building:
 class House(Building):
     def __init__(self, pFloors, pWidth, pHeight, pBedrooms, pBathrooms):
         super.__init__(pFloors, pWidth, pHeight)
-        self.bedrooms = pBedrooms
-        self.bathrooms = pBathrooms
+        self.__bedrooms = pBedrooms
+        self.__bathrooms = pBathrooms
 
